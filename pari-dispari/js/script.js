1d444e3914e1a272ab0c52ac8a7ddb40ebeb6e1f
cromeo98 <romeo.christian98@gmail.com>
2021-06-07 18:17:47 +0200

    correct and semplified prompt for even odd

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,15 +1,23 @@
 // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 // 1) Chiedo all'utente di scegliere tra pari e dispari
-var userEvenOdd = prompt('Scegli.. pari o dispari?')
+var userEvenOdd = prompt('Scegli.. pari o dispari?').toLowerCase();
 
-// 2) Trasformo la scelta in lower case
-var userEvenOddLowCase = userEvenOdd.toLowerCase()
+// 2) Verifico che l'utente abbia effetivamente scritto pari o dispari
+var i = 0;
 
-// 3) Verifico che l'utente abbia effetivamente scritto pari o dispari
-var i = 0
-
-while (userEvenOddLowCase != pari || userEvenOddLowCase != dispari){
-    word = prompt('Scrivi pari o dispari!');
-    i++;
+while (userEvenOdd != 'pari' && userEvenOdd != 'dispari'){
+    userEvenOdd = prompt('Scrivi pari o dispari!');
+    i++
 }
+
+// 4) Chiedo all'utente di scegliere tra pari e dispari
+//var userNumber = prompt('Scegli un numero tra 1 e 5');
+
+// 3) Verifico che l'utente abbia effetivamente scelto un numero tra 1 e 5
+//var i = 0;
+
+//while (userNumber > 5 || userNumber <= 0){
+//    userNumber = prompt('Scegli un numero tra 1 e 5!');
+//    i++;
+//}
