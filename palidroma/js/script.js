817500a892368e8b480184dfcfa9a0b50adaf939
cromeo98 <romeo.christian98@gmail.com>
2021-06-07 17:05:56 +0200

    add: .split turn string->array (for pos letters)

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -1,11 +1,15 @@
 // Palidroma Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
 
 // 1) Chiedo all'utente di inserire una parola (minimo 3 lettere)
-var word = prompt('inserisci una parola di minimo 3 lettere')
+var word = prompt('inserisci una parola di minimo 3 lettere');
 
-var i = 0
+var i = 0;
 
 while (word.length < 3){
-    word = prompt('inserisci una parola di MINIMO 3 lettere')
-    i++
+    word = prompt('inserisci una parola di MINIMO 3 lettere');
+    i++;
 }
+
+var splittedWord = word.split("");
+
+console.log(splittedWord);
