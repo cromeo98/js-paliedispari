4b9857ca58687c8337255ce07d76fcd7e336a6c5
cromeo98 <romeo.christian98@gmail.com>
2021-06-08 10:27:53 +0200

    correct randomNum function (max was escluded)

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,19 +1,21 @@
 // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 // 1) Chiedo all'utente di scegliere tra pari e dispari
-var userEvenOdd = prompt('Scegli.. pari o dispari?').toLowerCase();
+var userEvenOdd = prompt('Scegli.. pari o dispari?').toLowerCase;
 
 // 2) Verifico che l'utente abbia effetivamente scritto pari o dispari
 var i = 0;
 
 while (userEvenOdd != 'pari' && userEvenOdd != 'dispari'){
     userEvenOdd = prompt('Scrivi pari o dispari!');
-    i++
+    i++;
+    console.log(userEvenOdd);
 }
 console.log('L\'utente ha scelto ' + userEvenOdd);
 
 // 3) Chiedo all'utente di scegliere tra pari e dispari
 var userNumber = parseInt(prompt('Scegli un numero tra 1 e 5'));
+
 // 4) Verifico che l'utente abbia effetivamente scelto un numero tra 1 e 5
 var i = 0;
 
