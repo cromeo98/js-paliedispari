b17ccd576da3468a22958401a8108ff325b5db80
cromeo98 <romeo.christian98@gmail.com>
2021-06-08 11:40:20 +0200

    add: verify if userevenodd != null

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,9 +1,18 @@
 // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 // 1) Chiedo all'utente di scegliere tra pari e dispari
-var userEvenOdd = prompt('Scegli.. pari o dispari?').toLowerCase;
+var userEvenOdd = prompt('Scegli.. pari o dispari?');
 
-// 2) Verifico che l'utente abbia effetivamente scritto pari o dispari
+// 2a) Verifico che l'utente abbia effetivamente scritto qualcosa
+var i = 0
+while(userEvenOdd == null){
+    userEvenOdd = prompt('Scrivi pari o dispari!');
+    i++
+}
+
+userEvenOdd = userEvenOdd.toLowerCase
+
+// 2b) Verifico che l'utente abbia effetivamente scritto pari o dispari
 var i = 0;
 
 while (userEvenOdd != 'pari' && userEvenOdd != 'dispari'){
